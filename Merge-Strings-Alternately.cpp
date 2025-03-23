9ca720acadd72f9fefd8d974581262914cc3ad03
Mohammed yousry <yousry.arch@gmail.com>
2025-03-23 13:42:33 +0200

    [1768] [Time Beats: 43.66%] [Memory Beats: 63.06%] - LeetPush

---
+++ Merge-Strings-Alternately.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        std::string ans = "" ;
+        int i = 0 , j= 0 ; 
+        while (i< word1.size() || j< word2.size()) {
+             if (i<word1.size()) {
+                ans.push_back(word1[i]) ;
+                i++;
+             }
+             if (j< word2.size()) {
+                ans.push_back(word2[j]);
+                j++;
+             }
+        }
+        return ans ;
+        
+    }
+};
