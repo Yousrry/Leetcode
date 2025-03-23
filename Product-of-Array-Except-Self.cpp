aac064303e0b136329f1f965d620a819a21b6611
Mohammed yousry <yousry.arch@gmail.com>
2025-03-23 17:38:40 +0200

    Update Product-of-Array-Except-Self.cpp

--- Product-of-Array-Except-Self.cpp
+++ Product-of-Array-Except-Self.cpp
@@ -27,7 +27,7 @@ public:
     }
     */
 };
-
+         for(int i=0; i<nums.size(); i++) {
 /*            for (int j=0; j<nums.size();j++) {
                 if (j==i) 
                     continue ;
