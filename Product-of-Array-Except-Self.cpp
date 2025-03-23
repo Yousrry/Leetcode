53629939e3bc8494c4d5533fbe5e316f6b9b38cc
Mohammed yousry <yousry.arch@gmail.com>
2025-03-23 13:06:40 +0200

    [238] [Time Beats: 100.00%] [Memory Beats: 79.69%] - LeetPush

---
+++ Product-of-Array-Except-Self.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int mul=1 ;
+       std::vector <int> ans(nums.size()); 
+        for(int i=0; i<nums.size(); i++) {
+            ans[i] = mul ;
+            mul *= nums[i] ;
+            
+        }
+        mul =1;
+        for(int i=nums.size()-1; i>=0; i--) {
+            ans[i] *= mul ;
+            mul *= nums[i] ;
+                        
+        }
+        return ans ;
+    }
+        /*    
+        for(int i=0; i<nums.size(); i++) {
+            if (nums[i]==0)
+                continue;
+            else
+                ans[i] = ans[i]/nums[i] ;
+        }
+        return ans ;
+    }
+    */
+};
+
+/*            for (int j=0; j<nums.size();j++) {
+                if (j==i) 
+                    continue ;
+                else
+                    mul *= nums[j] ;
+            }
+            ans[i]= mul ;
+            mul =1 ;
+        }
+        return ans ;
+        
+    }*/
